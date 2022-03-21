a5b7aa356d2d64b52cbe1e823ec9d66241db1e2e
meng sieng <mengsieng020@gmail.com>
2022-03-21 14:34:17 +0700

    first commit

---
+++ lib/base_class.dart
@@ -0,0 +1,7 @@
+library base_class;
+
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+export './consumer_widget/base_stateless.dart';
+export './change_notifier/base_change_notifier.dart';
+export 'package:hooks_riverpod/hooks_riverpod.dart';
---
+++ lib/consumer_widget/base_stateless.dart
@@ -0,0 +1,35 @@
+import 'package:base_class/change_notifier/base_change_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:loader_overlay/loader_overlay.dart';
+
+/// You can use [BaseStateless] to connect with change notifier
+/// to controll state management easy and less code
+abstract class BaseStateless<T extends BaseChangeNotifier>
+    extends ConsumerWidget {
+  BaseStateless({Key? key}) : super(key: key) {
+    _changeNotifier = registerChangeNotifier();
+    _provider = ChangeNotifierProvider.autoDispose((ref) => _changeNotifier);
+  }
+
+  late final T _changeNotifier;
+  late final ProviderListenable<T> _provider;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    /// [LoaderOverlay] this widget used for show loading
+    return LoaderOverlay(
+      child: buildWidget(context, ref.watch(_provider)),
+    );
+  }
+
+  /// This [buildWidget] need to override when extends
+  /// This used for write UI design
+  Widget buildWidget(BuildContext context, T povider);
+
+  /// [registerChangeNotifier] for aassign changeNoifier instance
+  /// for get acess to changeNoifier that you want to register for using
+  @protected
+  T registerChangeNotifier();
+}
